68758e0b267226ffd9e696a7a956d9f632f830c5
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-05-16 15:47:06 +0900

    発展問題

---
+++ src/main/java/DbDevExam4_SelectSum.java
@@ -0,0 +1,70 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DbDevExam4_SelectSum {
+    public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        int sum = 0;
+        String result = "合計金額:";
+
+        try {
+            // load JDBC Driver
+            Class.forName("org.postgresql.Driver");
+
+            // confirm
+            System.out.println(" --- before connection --- ");
+
+            // database connect
+            con = DriverManager.getConnection("jdbc:postgresql:dbconnection", "axizuser", "axiz");
+
+            // confirm
+            System.out.println(" --- after connection --- ");
+
+            // SQL query string
+            String sql = "SELECT * FROM products ORDER BY product_id ASC";
+
+            // create statement
+            stmt = con.prepareStatement(sql);
+
+            // execute
+            ResultSet rs = stmt.executeQuery();
+
+            // output
+            while (rs.next()) {
+                String price = rs.getString("price");
+                
+                if(price != null && !price.isEmpty()) {
+                	sum += Integer.parseInt(price);
+                }
+                                         
+            }
+            
+            System.out.println(result + sum);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // close
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/DbDevExam4_Update.java
@@ -0,0 +1,62 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbDevExam4_Update {
+	public static void main(String[] args) {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        int param1 = 60;
+        int param2 = 101;
+
+        try {
+            // load JDBC Driver
+            Class.forName("org.postgresql.Driver");
+
+            // confirm
+            System.out.println(" --- before connection --- ");
+
+            // database connect
+            con = DriverManager.getConnection("jdbc:postgresql:dbconnection", "axizuser", "axiz");
+
+            // confirm
+            System.out.println(" --- after connection --- ");
+
+            // SQL query string
+            String sql = "UPDATE products SET product_id = ? WHERE product_id = ?";
+
+            // create statement
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, param1);
+            stmt.setInt(2, param2);
+
+            stmt.executeUpdate();
+            
+            System.out.println("更新しました。");
+
+            
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // close
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
