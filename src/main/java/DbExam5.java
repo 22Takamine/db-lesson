493ecb0ef9eef332be189c3976117d33fab09103
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-05-17 18:45:19 +0900

    基本問題

---
+++ src/main/java/DbExam5.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.util.List;
+
+import dao.ProductDao;
+import entity.Product;
+import util.DbUtil;
+
+public class DbExam5 {
+    public static void main(String[] args) {
+    	Connection connection;
+	   	ProductDao productDao;
+	   	Product data;
+	   	String textId = "product_id:";
+	   	String textName = "product_name:";
+	   	String textPrice = "price:";
+	   	String name = "ボールペン";
+	   	int price = 200;
+	   	 
+	   	connection = DbUtil.getConnection();
+	   	productDao = new ProductDao(connection);
+	   	
+	   	Product newProduct = new Product(name,price);
+        productDao.register(newProduct);
+	   	
+    	List<Product> list = productDao.findAll();
+        
+        for(int i = 0; i < list.size(); i++) {
+        	data = list.get(i);
+        	
+        	System.out.print(textId + data.getProductId() + ",");
+        	System.out.print(textName +data.getProductName() + ",");
+        	System.out.println(textPrice + data.getPrice());
+        	
+        }
+    }
+    
+}
---
+++ src/main/java/dao/ProductDaoTest.java
@@ -0,0 +1,134 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import entity.Product;
+import util.DbUtil;
+
+public class ProductDaoTest {
+
+    private Connection connection;
+    private ProductDao productDao;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        connection = DbUtil.getConnection();
+        connection.setAutoCommit(false);
+
+        try (PreparedStatement stmt = connection.prepareStatement("DELETE FROM products")) {
+            stmt.executeUpdate();
+        }
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.addBatch("INSERT INTO products VALUES (101,'鉛筆', 50)");
+            stmt.addBatch("INSERT INTO products VALUES (102,'消しゴム', 100)");
+            stmt.addBatch("INSERT INTO products VALUES (103,'地球儀', 5000)");
+            stmt.addBatch("INSERT INTO products VALUES (104,'ボールペン', 200)");
+            stmt.executeBatch();
+        }
+
+        productDao = new ProductDao(connection);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        connection.rollback();
+    }
+
+    @Test
+    public void findAllで全件取得できる() {
+        List<Product> list = productDao.findAll();
+        assertEquals(4, list.size());
+
+        Product u = list.get(0);
+        assertEquals(Integer.valueOf(101), u.getProductId());
+        assertEquals("鉛筆", u.getProductName());
+        assertEquals(Integer.valueOf(50), u.getPrice());
+
+        u = list.get(1);
+        assertEquals(Integer.valueOf(102), u.getProductId());
+        assertEquals("消しゴム", u.getProductName());
+        assertEquals(Integer.valueOf(100), u.getPrice());
+    }
+    
+    @Test
+    public void findAllはデータがないと空のリストを返す() throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement("DELETE FROM products")) {
+            stmt.executeUpdate();
+        }
+
+        List<Product> list = productDao.findAll();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void findByProductIdで存在するデータが正しく取得できる() {
+        Product product = productDao.findByProductId(101);
+        assertEquals(Integer.valueOf(101), product.getProductId());
+        assertEquals("鉛筆", product.getProductName());
+        assertEquals(Integer.valueOf(50), product.getPrice());
+    }
+
+    @Test
+    public void findByIdで存在しないデータはnullになる() {
+        Product product = productDao.findByProductId(10);
+        assertNull(product);
+    }
+
+    @Test
+    public void insertでデータを登録できる() {
+        Product newProduct = new Product(10, "test",10);
+        productDao.insert(newProduct);
+
+        Product getProduct = productDao.findByProductId(10);
+        assertEquals(newProduct.getProductId(), getProduct.getProductId());
+        assertEquals(newProduct.getProductName(), getProduct.getProductName());
+    }
+
+    @Test
+    public void insertで主キーが重複していると例外発生() {
+        Product newProduct = new Product(101, "鉛筆", 50);
+        assertThrows(RuntimeException.class, () -> productDao.insert(newProduct));
+    }
+
+    @Test
+    public void updateでデータを更新できる() {
+        Product product = productDao.findByProductId(101);
+        assertEquals(Integer.valueOf(101), product.getProductId());
+        assertEquals("鉛筆", product.getProductName());
+        assertEquals(Integer.valueOf(50), product.getPrice());
+
+        //product.setProductName("test2");
+        product.setPrice(60);
+        productDao.update(product);
+
+        product = productDao.findByProductId(101);
+        assertEquals(Integer.valueOf(101), product.getProductId());
+        assertEquals("鉛筆",product.getProductName());
+        assertEquals(Integer.valueOf(60), product.getPrice());
+    }
+
+    @Test
+    public void deleteでデータを削除できる() {
+        Product product = productDao.findByProductId(101);
+        assertNotNull(product);
+
+        productDao.delete(101);
+
+        product = productDao.findByProductId(101);
+        assertNull(product);
+    }
+    
+
+
+}
